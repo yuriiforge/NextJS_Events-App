0bc9925be2cea27caec342fbc4e8bbc60c814c33
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-25 14:09:47 +0300

    Connect app to Firebase. Create events array in DB

---
+++ helpers/api-util.js
@@ -0,0 +1,25 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://nextjs-events-9e1cc-default-rtdb.europe-west1.firebasedatabase.app/.json'
+  );
+  const data = await response.json();
+
+  console.log(data);
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-
+const HomePage = (props) => {
   return (
     <div>
-      <EventList events={featuredEvents} />
+      <EventList events={props.featuredEvents} />
     </div>
   );
 };
+
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
+}
 
 export default HomePage;
