d7b35e498eb3485be992ea625e577c293ddf84cd
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-25 10:49:27 +0300

    Implement AllEventsPage

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 const AllEventsPage = () => {
+  const allEvents = getAllEvents();
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList events={allEvents} />
     </div>
   );
 };
