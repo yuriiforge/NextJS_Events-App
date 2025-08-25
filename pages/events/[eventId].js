072c1389d155085684419349f615d2b9873d0fd3
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-25 14:18:44 +0300

    Implement single event static page fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,18 +1,12 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/UI/error-alert';
 
-const EventDetailPage = () => {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-
-  const event = getEventById(eventId);
-
-  if (!event) {
+const EventDetailPage = (props) => {
+  if (!props.selectedEvent) {
     return (
       <ErrorAlert>
         <p>No event found.</p>
@@ -22,18 +16,40 @@ const EventDetailPage = () => {
 
   return (
     <>
-      <EventSummary title={event.title} />
+      <EventSummary title={props.selectedEvent.title} />
       <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
+        date={props.selectedEvent.date}
+        address={props.selectedEvent.location}
+        image={props.selectedEvent.image}
+        imageAlt={props.selectedEvent.title}
       />
       <EventContent>
-        <p>{event.description}</p>
+        <p>{props.selectedEvent.description}</p>
       </EventContent>
     </>
   );
 };
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default EventDetailPage;
