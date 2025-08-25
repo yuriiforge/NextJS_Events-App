b9d1c1339f6943fae24c01480cb2ca0fe78f5ab2
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-25 10:28:55 +0300

    Create basic layout for Events page

---
+++ components/events/event-item.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+import classes from './event-item.module.css';
+
+const EventItem = ({ event }) => {
+  const { id, image, date, location, title } = event;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exporeLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Link href={exporeLink}>Expore event</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
---
+++ components/events/event-list.js
@@ -0,0 +1,15 @@
+import EventItem from './event-item';
+
+import classes from './event-list.module.css';
+
+const EventList = ({ events }) => {
+  return (
+    <ul className={classes.list}>
+      {events.map((event) => (
+        <EventItem key={event.id} event={event} />
+      ))}
+    </ul>
+  );
+};
+
+export default EventList;
