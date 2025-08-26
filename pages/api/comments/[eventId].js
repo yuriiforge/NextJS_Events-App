011bbaee3b2521cfacb550665baaa9d10751748b
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-26 12:39:31 +0300

    Add API for comments

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,39 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input' });
+      return;
+    }
+
+    console.log(email, name, text);
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
+  }
+
+  if (req.method === 'GET') {
+    const dummyList = [
+      { id: 'c1', name: 'Yurii', text: 'A first comment' },
+      { id: 'c2', name: 'Yurii', text: 'A second comment' },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
+
+export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -4,6 +4,7 @@ function handler(req, res) {
 
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address.' });
+      return;
     }
 
     console.log(userEmail);
