bdb21974326ccff54e5eec5d66de067b8f0a4c7e
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-26 13:12:51 +0300

    Create and connect MongoDB. Create User collection. Save user to DB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,9 @@
-function handler(req, res) {
+import dbConnect from '../../lib/mongodb';
+import User from '../../models/User';
+
+async function handler(req, res) {
+  await dbConnect();
+
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,8 +12,26 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({ message: 'Signed up!' });
+    try {
+      const existingUser = await User.findOne({ email: userEmail });
+
+      if (existingUser) {
+        res.status(409).json({ message: 'Email already registered.' });
+        return;
+      }
+
+      const newUser = await User.create({ email: userEmail });
+
+      res.status(201).json({
+        message: 'Signed up!',
+        user: newUser,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
 }
 
